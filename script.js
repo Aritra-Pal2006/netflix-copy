2ee78a31a593512783c4e29a747c9f8501a403bb
Aritra-Pal2006 <aritrapalofficial2006@gmail.com>
2025-06-27 23:31:01 +0530

    navbar completed

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function change()
 
     }
 }
+
+const magnify=document.getElementById('Search');
+magnify.addEventListener('click',reveal)
+function reveal()
+{
+    if(document.getElementById('input-box').classList.contains('hidden')){
+
+        document.getElementById('input-box').classList.remove('hidden');
+        document.getElementById('input-box').focus();
+
+    }
+    else{
+
+        document.getElementById('input-box').classList.add('hidden');
+
+    }
+}
+
